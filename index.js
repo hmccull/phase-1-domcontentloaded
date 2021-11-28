109cbb8e43b75e2715e15f2c3b1595d26b6f38af
Hannah McCullough <hmac091@gmail.com>
2021-11-28 12:42:37 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+// create event listener to detect when the HTML page has loaded
+document.addEventListener('DOMContentLoaded', function() {
+    replaceText();
+});
+
+// callback function to create p element with id='text' and replace text
+function replaceText() {
+    // locate p element w/ id='text'
+    let targetText = document.getElementById('text');
+    // replace text value 
+    targetText.textContent = 'This is really cool!';
+}
